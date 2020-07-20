b22a48033489094a27d90ccfcd430093bcf21a11
e <leegradylll@gmail.com>
2020-07-20 16:23:09 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace FunnyDation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var viewModelName = $"{viewName}Vm, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+        }
+    }
+}
---
+++ MainWindowVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnyDation
+{
+    /// <summary>
+    /// MainWindowVm
+    /// </summary>
+    public class MainWindowVm
+    {
+        public MainWindowVm()
+        {
+
+        }
+    }
+}
