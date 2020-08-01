7b7cdaaef74b5e280755cfa63a173ca645afd08a
e <leegradylll@gmail.com>
2020-08-01 23:25:53 +0800

    back to framework

---
+++ FunnyDation.Start/App.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Prism.Unity;
+
+namespace FunnyDation.Start
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var win = Container.Resolve<MainWindow>();
+            win.WindowState = WindowState.Maximized;
+            win.Show();
+            return win;
+        }
+        /// <summary>
+        /// IOC
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+
+
+            //  FDIoc.ServiceProvider = new UnityServiceLocatorAdapter(containerRegistry.GetContainer());
+        }
+
+        /// <summary>
+        /// init Module
+        /// </summary>
+        /// <param name="moduleCatalog"></param>
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            string mdPath = Path.Combine(@"C:\Users\Ricov\source\repos\FunnyDation\Assembly\", "Modules");
+            var files = Directory.GetFiles(mdPath, "FunnyDation.*.Wpf.dll", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                var dll = Assembly.LoadFile(file);
+                var types = dll.GetTypes();
+                var modules = types.Where(p => p.GetInterfaces().Contains(typeof(IModule)));
+
+                foreach (var module in modules)
+                {
+                    moduleCatalog.AddModule(new ModuleInfo()
+                    {
+                        ModuleName = module.FullName,
+                        ModuleType = module.AssemblyQualifiedName,
+                        Ref = new Uri(file, UriKind.RelativeOrAbsolute).AbsoluteUri
+
+                    });
+                }
+            }
+
+            base.ConfigureModuleCatalog(moduleCatalog);
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var viewModelName = $"{viewName}Vm, {viewAssemblyName}";
+                return Type.GetType(viewModelName);
+            });
+
+            base.ConfigureViewModelLocator();
+
+        }
+    }
+}
