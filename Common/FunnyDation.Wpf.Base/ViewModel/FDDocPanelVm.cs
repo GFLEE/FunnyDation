7bf7e3c489953b6c006d4021416f0ee95f062795
e <leegradylll@gmail.com>
2020-08-02 17:29:36 +0800

    doc panel

---
+++ Common/FunnyDation.Wpf.Base/ViewModel/FDDocPanelVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyDation.Wpf.Base.ViewModel
+{
+    public class FDDocPanelVm
+    {
+        public FDDocPanelVm()
+        {
+
+        }
+
+
+
+
+
+    }
+}
