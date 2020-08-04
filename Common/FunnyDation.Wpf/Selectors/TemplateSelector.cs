303e2122ddc0539457ad80708551d3fe8326a62d
e <leegradylll@gmail.com>
2020-08-04 19:01:17 +0800

    rib

--- Common/FunnyDation.Wpf/Selectors/TemplateSelector.cs
+++ Common/FunnyDation.Wpf/Selectors/TemplateSelector.cs
@@ -8,15 +8,49 @@ namespace FunnyDation.Wpf.Selectors
 {
     public class TemplateSelector : DataTemplateSelector
     {
-        public object Host { get; set; }
+        public object Host { get; set; } 
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+
+            var templateItem = item as ITemplateItem;
+            var tName = DefaultTemplateName;
+            if (templateItem != null && !string.IsNullOrEmpty(templateItem.TemplateName))
+            {
+                tName = templateItem.TemplateName;
+
+            }
+
+            if (string.IsNullOrEmpty(tName))
+            {
+                return null;
+            }
+
+            if (container is FrameworkElement)
+            {
+                DataTemplate result = (DataTemplate)((FrameworkElement)container).FindResource(tName);
+
+                return result;
+            }
+            if (container is FrameworkContentElement)
+            {
+                DataTemplate result = (DataTemplate)((FrameworkContentElement)container).FindResource(tName);
+                if (result == null)
+                {
+                    if (this.Host != null)
+                    {
+                        result = (DataTemplate)((FrameworkElement)Host).FindResource(tName);
+
+                    }
+                }
+                return result;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
 
 
-        //public override SelectTemplate(object item,DependencyObject container)
-        //{
-
-        //}
-
-
+        public string DefaultTemplateName { get; set; }
 
     }
 }
