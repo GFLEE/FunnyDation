5537d1ee50979cbbd5a556e16380ab798d04aeee
e <leegradylll@gmail.com>
2020-08-11 13:29:19 +0800

    documentpanel

---
+++ Common/FunnyDation.Wpf.Base/ViewModel/DocPanel/DockPanelVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyDation.Wpf.Base.ViewModel.DocPanel
+{
+    public class DockPanelVm
+    {
+        public string TemplateName { get; set; }
+    }
+}
---
+++ Common/FunnyDation.Wpf.Devexpress/Selector/DockPanelSelector.cs
@@ -0,0 +1,28 @@
+﻿using FunnyDation.Wpf.Base.ViewModel.DocPanel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FunnyDation.Wpf.Devexpress.Selector
+{
+    public class DockPanelSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var vm = item as DockPanelVm;
+            if (vm == null || string.IsNullOrEmpty(vm.TemplateName))
+            {
+                return null;
+            }
+
+            var result = (DataTemplate)((Control)container).FindResource((vm.TemplateName));
+
+            return result; 
+        } 
+
+    }
+}
