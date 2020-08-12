be5fe8c93b64a3537119a8005b618cdd44faf799
e <leegradylll@gmail.com>
2020-08-12 10:31:31 +0800

    fake finder

---
+++ Common/FunnyDation.Wpf.Base/FakeFinder.cs
@@ -0,0 +1,4 @@
+﻿namespace FunnyDation.Wpf.Base
+{
+ 
+}
--- Common/FunnyDation.Wpf.Base/VmLinkVm.cs
+++ Common/FunnyDation.Wpf.Base/VmLinkVm.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -103,13 +104,96 @@ namespace FunnyDation.Wpf.Base
                     SetProperty(ref sourceVm, value);
                 }
 
-                 
+
             }
         }
-
+        /// <summary>
+        /// OnSourceVmChanged
+        /// </summary>
         public virtual void OnSourceVmChanged()
         {
 
         }
+
+        /// <summary>
+        /// Rela Object Invoke
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="param"></param>
+        public virtual void CallBack(string name, object param)
+        {
+
+        }
+
+        /// <summary>
+        /// 查找SourceVm
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T FindSourceVm<T>() where T : class
+        {
+            var parent = SourceVm;
+            while (parent != null)
+            {
+                if (parent is T)
+                {
+                    return (T)parent;
+                }
+                if (parent is VmLinkVm)
+                {
+                    continue;
+                }
+                parent = FakeFind(parent);
+                if (parent == null)
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+        static object FakeFind(object sourceVm)
+        {
+            foreach (var finder in finders)
+            {
+                if (finder.Check(sourceVm))
+                {
+                    var linkVm = finder.Find(sourceVm);
+                    return linkVm;
+                }
+            }
+
+            return null;
+
+        }
+        static List<FakeFinder> finders = new List<FakeFinder>();
+
+        /// <summary>
+        /// 注册可以类似SourceVm查找的委托
+        /// </summary>
+        /// <param name="finder"></param>
+        internal static void RegisterFindFakeSourceVm(FakeFinder finder)
+        {
+            finders.Add(finder);
+        }
+        public virtual string VmDataKey { get; set; }
+        internal class FakeFinder
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="find">查找的委托</param>
+            /// <param name="check">是否应用“查找的委托”</param>
+            public FakeFinder(Func<object, object> find, Func<object, bool> check)
+            {
+                this.Find = find;
+                this.Check = check;
+            }
+
+            public Func<object, object> Find { get; set; }
+            public Func<object, bool> Check { get; set; }
+        }
+
     }
 }
