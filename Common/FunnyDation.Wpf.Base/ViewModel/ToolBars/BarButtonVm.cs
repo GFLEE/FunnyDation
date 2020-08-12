87990f0a991a1d4c6fa4d09d1dc3fc2e194a2300
e <leegradylll@gmail.com>
2020-08-12 17:19:55 +0800

    modify BarButtonVm

--- Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarButtonVm.cs
+++ Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarButtonVm.cs
@@ -7,26 +7,102 @@ using System.Windows;
 
 namespace FunnyDation.Wpf.Base.ViewModel.ToolBars
 {
-    public class BarButtonVm
-    {
-        private string toolTip;
-        private string glyphName;
-        private Visibility visibility;
-
-        public BarButtonVm()
+    public class BarButtonVm : BarItemVm
+    { 
+        public BarButtonVm(string toolTip, string glyphName) : base(toolTip)
         {
-
+            this.TemplateName = "TemplateButton";
+            this.GlyphName = glyphName;
         }
 
-        public BarButtonVm(string toolTip, string glyphName, Visibility visibility)
+        public BarButtonVm(string toolTip, string glyphName, Visibility visibility) : base(toolTip)
         {
-            this.toolTip = toolTip;
-            this.glyphName = glyphName;
-            this.visibility = visibility;
+            this.TemplateName = "TemplateButton";
+            this.GlyphName = glyphName;
+            this.Visibility = visibility;
+            this.BtnText = toolTip;
+            if (ToolTip != null)
+            {
+                ToolTip = ToolTip.Replace("\n", "").Replace(" ", "").Replace("\t", "").Replace("\r", "");
+            }
         }
 
-        public string DataKey { get; internal set; }
-        public string Name { get; internal set; }
-        public string LargeGlyphName { get; internal set; }
+
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public string largeGlyphName;
+        public string LargeGlyphName
+        {
+            get
+            {
+                return largeGlyphName;
+            }
+
+            set
+            {
+                largeGlyphName = value;
+                SetProperty(ref largeGlyphName, value);
+
+            }
+        }
+
+        /// <summary>
+        /// 显示隐藏
+        /// </summary>
+        public Visibility visibility = Visibility.Visible;
+        public Visibility Visibility
+        {
+            get
+            {
+                return visibility;
+            }
+
+            set
+            {
+                visibility = value;
+                SetProperty(ref visibility, value);
+
+            }
+        }
+
+        /// <summary>
+        /// 显示与否
+        /// </summary>
+        public bool isEnableBtn;
+        public bool IsEnableBtn
+        {
+            get
+            {
+                return isEnableBtn;
+            }
+
+            set
+            {
+                isEnableBtn = value;
+                SetProperty(ref isEnableBtn, value);
+
+            }
+        }
+
+        /// <summary>
+        /// Button Text
+        /// </summary>
+        public string btnText;
+        public string BtnText
+        {
+            get
+            {
+                return btnText;
+            }
+
+            set
+            {
+                btnText = value;
+                SetProperty(ref btnText, value);
+
+            }
+        }
+     
     }
 }
--- Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarItemVm.cs
+++ Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarItemVm.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace FunnyDation.Wpf.Base.ViewModel.ToolBars
 {
-    public class BarItemVm:NodeVm
+    /// <summary>
+    /// BarItemVm
+    /// </summary>
+    public class BarItemVm : NodeVm
     {
+        public BarItemVm(string toolTip) : base()
+        {
+            ToolTip = toolTip;
+        }
+
+        public ToolBarVm ToolBar
+        {
+            get
+            {
+                return (ToolBarVm)this.Container;
+            }
+        }
+        public string ToolTip { get; set; }
     }
 }
