c8b033b2429d749b7aff7e059e03e9a2a21992d1
e <leegradylll@gmail.com>
2020-08-28 10:50:33 +0800

    toolba rbase

---
+++ Common/FunnyDation.Common/Arguments/Param1Args.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnyDation.Common
+{
+    public class Param1Args<T> : EventArgs
+    {
+        public Param1Args(T pram)
+        {
+            this.Param = pram;
+        }
+
+        public T Param { get; set; }
+    }
+}
--- Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarCheckVm.cs
+++ Common/FunnyDation.Wpf.Base/ViewModel/ToolBars/BarCheckVm.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using FunnyDation.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FunnyDation.Wpf.Base.ViewModel.ToolBars
 {
-    class BarCheckVm
+    public class BarCheckVm : BarItemVm
     {
+        public BarCheckVm(string dataKey, string text, string toolTip) : base(toolTip)
+        {
+            this.TemplateName = "TemplateTextEdit";
+            this.Text = text;
+        }
+
+
+        public string text;
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+                SetProperty(ref text, value);
+
+            }
+        }
+
+        public bool isCheck;
+        public bool IsCheck
+        {
+            get
+            {
+                return isCheck;
+            }
+
+            set
+            {
+                isCheck = value;
+                OnChecked();
+                SetProperty(ref isCheck, value);
+
+            }
+        }
+
+        public event EventHandler<Param1Args<object>> OnCheckedChanged;
+        private void OnChecked()
+        {
+            OnCheckedChanged(this, new Param1Args<object>(IsCheck));
+
+        }
+
+        /// <summary>
+        /// 显示隐藏
+        /// </summary>
+        public Visibility visibility = Visibility.Visible;
+        public Visibility Visibility
+        {
+            get
+            {
+                return visibility;
+            }
+
+            set
+            {
+                visibility = value;
+                SetProperty(ref visibility, value);
+
+            }
+        }
+
+
+
     }
 }
